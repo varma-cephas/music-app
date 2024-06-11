52a54ef5c1a447a4f19787a17254ef71d816e6ca
varmac <varmac231@gmail.com>
2024-06-11 06:08:13 +0000

    debugging buttons - fix audio path url

--- js/app.js
+++ js/app.js
@@ -11,7 +11,8 @@ const artist = document.querySelector(".artist");
 const song_name = document.querySelector(".song-name")
 
 const play_icon_container = document.querySelector(".play-icon-container");
-let music_arr = ["assets/music/01_Vries_Lose_Traction.mp3","assets/music/02_Shawn_Pink_Type_Writer.mp3", "assets/music/03_A_Ura_Lu_No_Reply.mp3", "assets/music/04_Bax_Iv_Perspective.mp3", "assets/music/05_Soft_Clouds_Falling_Behind.mp3"];
+let music_arr = ["01_Vries_Lose_Traction.mp3","02_Shawn_Pink_Type_Writer.mp3", "03_A_Ura_Lu_No_Reply.mp3", "04_Bax_Iv_Perspective.mp3", "05_Soft_Clouds_Falling_Behind.mp3"];
+// let music_arr = ["assets/music/abc/01_Vries_Lose_Traction.mp3","assets/abc/music/02_Shawn_Pink_Type_Writer.mp3", "assets/music/abc/03_A_Ura_Lu_No_Reply.mp3", "assets/music/abc/04_Bax_Iv_Perspective.mp3", "assets/music/abc/05_Soft_Clouds_Falling_Behind.mp3"];
 
 // const music_player_duration = document.querySelector(".music-player-duration");
 
@@ -32,16 +33,15 @@ play_skip_forward.addEventListener("click",()=>{
     for(let x = 0; x < music_arr.length; x++){
         if(`${audio_player.src.split("/")[6]}` === music_arr[x]){
             if(`${audio_player.src.split("/")[6]}` === music_arr[5]){
-                audio_player.setAttribute("src", music_arr[0])
+                audio_player.setAttribute("src", `assets/music/${music_arr[0]}`)
             }else{
                 keepVal++
                 if(keepVal > 4){
                     keepVal = 0;
                 }
-                console.log('here')
-                audio_player.setAttribute("src", music_arr[keepVal]);
+                audio_player.setAttribute("src", `assets/music/${music_arr[keepVal]}`);
                 audio_player.autoplay = true;
-                artist.textContent = `${audio_player.src.split("/")[5].split("_")[1]}`;
+                artist.textContent = `${audio_player.src.split("/")[6].split("_")[1]}`;
                 song_name.textContent = `${audio_player.src.split("/")[6].split("_")[2]}`;
                 break;
             }
@@ -56,14 +56,13 @@ play_skip_back.addEventListener("click", ()=>{
     for(let x = 0; x < music_arr.length; x++){
         if(`${audio_player.src.split("/")[6]}` === music_arr[x]){
             if(`${audio_player.src.split("/")[6]}` === music_arr[0]){
-                audio_player.setAttribute("src", music_arr[music_arr.length]);
+                audio_player.setAttribute("src", `assets/music/${music_arr[4]}`);
             }else{
                 keepVal--
                 if(keepVal === 0){
                     keepVal = 4;
                 }
-                console.log('heresfdd')
-                audio_player.setAttribute("src", music_arr[keepVal]);
+                audio_player.setAttribute("src", `assets/music/${music_arr[keepVal]}`);
                 audio_player.autoplay = true;
                 artist.textContent =  audio_player.src.split("/")[6].split("_")[1];
                 song_name.textContent = `${audio_player.src.split("/")[6].split("_")[2]}`;
